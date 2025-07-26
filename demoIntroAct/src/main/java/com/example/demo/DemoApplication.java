e1226e2a8f7c8b63f62ee1222c2fbcc7158e0eec
ray <togneriraony@gmail.com>
2025-07-26 12:41:36 -0300

    post/get funcionando e integrados à  página de atividades! Tela de erro adicionada

--- demoIntroAct/src/main/java/com/example/demo/DemoApplication.java
+++ demoIntroAct/src/main/java/com/example/demo/DemoApplication.java
@@ -1,31 +1,12 @@
 package com.example.demo;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController
 public class DemoApplication {
 	// http://localhost:8080/
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
-	}
-
-	@Autowired
-	private ThemeRepository themeRepository;
-
-	@PostMapping("/post")
-	public void salvaTemas(@RequestBody Tema tema) {
-		themeRepository.save(tema);
-	}
-
-	@GetMapping("/get")
-	public Iterable<Tema> obtemTema() {
-		return themeRepository.findAll();
 	}
 }
--- demoIntroAct/src/main/java/com/example/demo/NavController.java
+++ demoIntroAct/src/main/java/com/example/demo/NavController.java
@@ -1,6 +1,6 @@
 package com.example.demo;
 
-import java.util.Iterator;
+import java.util.List;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -8,14 +8,20 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class NavController {
-    @GetMapping("/index")
+    private final ThemeService themeService;
+
+    public NavController(ThemeService themeService) {
+        this.themeService = themeService;
+    }
+
+    @GetMapping("/index")   
     public String index() {
         return "index"; // carrega templates/index.html
     }
 
     @GetMapping("/atividades")
     public String atividades(Model model) {
-        Iterator<Tema> temas = this.obtemTema().iterator();
+        List<Tema> temas = (List<Tema>) this.themeService.obtemTema();
         model.addAttribute("temas", temas);
         return "atividades"; // carrega templates/atividades.html
     }
