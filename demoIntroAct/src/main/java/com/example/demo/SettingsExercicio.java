4928ef8121d2be83ae7bc569883a446bd7307173
Ray <32366081+raonytog@users.noreply.github.com>
2025-07-28 10:52:24 -0300

    Remove SettingsExercicio

--- demoIntroAct/src/main/java/com/example/demo/SettingsExercicio.java
+++
@@ -1,5 +0,0 @@
-package com.example.demo;
-
-import java.util.List;
-
-public record SettingsExercicio(String idExercicio, String enunnciado, Integer dificuldade, Integer tipo, List<String> alternativas, List<String> gabarito) { }
