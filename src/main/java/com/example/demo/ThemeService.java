f5768e28a9902d20e0c204928c747b94ad5605ff
ray <togneriraony@gmail.com>
2025-08-12 20:43:54 -0300

    post com lista, http request para deleteAll

--- src/main/java/com/example/demo/ThemeService.java
+++ src/main/java/com/example/demo/ThemeService.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -19,12 +20,18 @@ public class ThemeService {
     }
 
     @PostMapping("/post")
-	public void salvaTemas(@RequestBody Tema tema) {
-		themeRepository.save(tema);
+	public void salvaTemas(@RequestBody List<Tema> temas) {
+		for (Tema l : temas)
+			themeRepository.save(l);
 	}
 
 	@GetMapping("/get")
 	public List<Tema> obtemTemas() {
 		return (List<Tema>) themeRepository.findAll();
+	}
+
+	@DeleteMapping("/delete")
+	public void deletaTemas() {
+		themeRepository.deleteAll();
 	}
 }
